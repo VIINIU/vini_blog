61971602e1c5d97c0ca20dc5b0ca3768a75da445
Yubin Seo | 서유빈 <139994047+VIINIU@users.noreply.github.com>
2026-05-21 18:58:22 +0900

    Add metadata for blog title and description
    Added metadata for title and description to RootLayout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,15 @@ import Footer from '@/app/components/Footer';
 import LeftBar from '@/app/components/LeftBar';
 import RightBar from '@/app/components/RightBar';
 
-import { RightBarProvider } from '@/app/components/RightBarContext'; // 새로 만든 Context Provider 임포트
+import { RightBarProvider } from '@/app/components/RightBarContext'; 
+
+export const metadata: Metadata = {
+  title: {
+    default: "VINI BLOG",     
+    template: "%s | VINI BLOG", 
+  },
+  description: "VINI의 Tech blog",
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
